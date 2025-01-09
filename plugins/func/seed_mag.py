3da10d78b724dddae48278645c145cbb0049ab97
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 11:24:10 +0530

    Update seed_mag.py

--- plugins/func/seed_mag.py
+++ plugins/func/seed_mag.py
@@ -1,1 +1,8 @@
+from bot import seedr
 
+def seed_file(maglink):
+  if seedr.check_session():
+    js = seedr.add_magnet(maglink)
+    if js and js["success"] == True:
+      folder_id=js["id"]
+      while seedr.get_folder_items().
