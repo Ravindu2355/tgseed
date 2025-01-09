c1cbee6c15db6c2be5101ebe716374daf8ceb50a
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 08:31:27 +0530

    Update simples.py

--- plugins/func/simples.py
+++ plugins/func/simples.py
@@ -1,5 +1,5 @@
 import urllib.parse
-
+from moviepy.editor import VideoFileClip
 
 def url_decode(encoded_string):
     return urllib.parse.unquote(encoded_string)
@@ -14,3 +14,11 @@ def mention_user(message:Message):
     mention = f"[{user_name}](tg://user?id={user_id})"
     return mention
 
+def generate_thumbnail(video_path, thumbnail_path):
+    try:
+        video = VideoFileClip(video_path)
+        video.save_frame(thumbnail_path, t=1.0)  # Save thumbnail at 1 second
+        return thumbnail_path
+    except Exception as e:
+        print(f"Error generating thumbnail: {e}")
+        return None
