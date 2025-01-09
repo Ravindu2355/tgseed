4b1dda9b96f07292a708cb1f9969621455197b9d
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 08:30:44 +0530

    Update simples.py

--- plugins/func/simples.py
+++ plugins/func/simples.py
@@ -1,1 +1,16 @@
+import urllib.parse
+
+
+def url_decode(encoded_string):
+    return urllib.parse.unquote(encoded_string)
+
+def url_encode(string):
+    return urllib.parse.quote(string)
+
+def mention_user(message:Message):
+    user = message.from_user
+    user_name = user.first_name
+    user_id = user.id
+    mention = f"[{user_name}](tg://user?id={user_id})"
+    return mention
 
