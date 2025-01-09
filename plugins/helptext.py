e994301ec44cd580f2bd026f43f9fece11a8e11c
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 08:42:19 +0530

    Update helptext.py

--- plugins/helptext.py
+++ plugins/helptext.py
@@ -5,11 +5,11 @@ from plugins.func.simples import mention_user
 @Client.on_message(filters.command("start"))
 async def _ms(client, message:types.Message):
     await message.reply(
-      f"**Rx Seedr Torrent Uper**\n\n**Hi🫡...{mention_user(messag)}😍\n\nGive me a Torrent Link or magnet link for upload**"
+      f"**💫Rx Seedr Torrent Uper💫**\n\n**Hi🫡...{mention_user(messag)}😍\n\nGive me a Torrent Link or magnet link for upload**"
     )
 
 @Client.on_message(filters.command("help"))
 async def _mh(client, msg:types.Message):
   await msg.reply(
-    f"**Rx Seedr Torrent Uper**\n\n💢This is a bot who made for download/seed torrents to directly to telegram...\n\n💘Please remember that this is only for its authers🫡...\n\nIf you got eny Unexpected dissaponment,\nSorry about that🥲..."
+    f"**💫Rx Seedr Torrent Uper💫**\n\n💢This is a bot who made for download/seed torrents to directly to telegram...\n\n💘Please remember that this is only for its authers🫡...\n\nIf you got eny Unexpected dissaponment,\nSorry about that🥲..."
   )
