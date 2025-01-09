8efa2cc9cabca1d078a65194bd1a486dfaaea0d4
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 09:37:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import time
 from pyrogram import Client, filters ,types
 from config import Config
+from tor.seed import SeedrClient
+from lg import logger as l
 
 API_ID = Config.apiid
 API_HASH = Config.apihash
@@ -11,7 +13,13 @@ BOT_TOKEN = Config.tk
 plugins = dict(root="plugins")
 bot = Client(name="RVX_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=plugins)
 
-
+#seedr client
+seedr = SeedrClient(
+    Config.seedr_email,
+    Config.seedr_pw
+)
+l.info("seedr client and pyrogram client was created")
 # Run the bot
 if __name__ == '__main__':
+    l.info("bot client starting...")
     bot.run()
