9f3e4c21c93914801031f24cbd93036467b1ce77
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-01-09 08:01:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config(object):
+  apiid = int(os.getenv("apiid"))
+  
+  apihash = os.getenv("apihash")
+  
+  tk = os.getenv("tk")
+  
+  AUTH = os.getenv("auth")
+  
+  OWNER =os.getenv("owner")
+
+  PRARAL_LIMIT = int(os.getenv("pps",3))
