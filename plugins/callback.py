8d5eb136f169cfff20ab591ab4b39419dca4c4bd
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-04-18 17:08:37 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 from bot import seedr
+from plugins.torfeed import magnet_button
+
 
 @Client.on_callback_query()
 async def callback_query_handler(client, callback_query):
@@ -9,3 +11,8 @@ async def callback_query_handler(client, callback_query):
           await callback_query.message.edit_text(f"Deleted {type} with ID: {file_id}")
         else:
           await callback_query.message.edit_text("Sorry system err!...")
+    elif callback_query.data.startswith("mgt_"):
+        await magnet_button(client, callback_query)
+    else:
+        await callback_query.message.edit_text("not defined callback....!")
+        
